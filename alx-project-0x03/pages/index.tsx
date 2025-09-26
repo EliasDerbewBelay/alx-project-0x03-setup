306f7a0fdef1aea561e6220edfc85c31af398323
Elias Derbew <eliasderbew1@gmail.com>
2025-09-26 14:43:13 +0300

    Home component impliment

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -1,7 +1,7 @@
 const Home: React.FC = () => {
   return (
     <div className="min-h-screen flex justify-center items-center">
-      <h1>Welecom To Splash App</h1>
+      <h1> Welcome to Splash App </h1>
     </div>
   );
 };
